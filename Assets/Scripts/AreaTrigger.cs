70dec409ce2a43515b0a94fb504a0f02251aefbb
Julia Sporre <>
2017-11-15 17:34:59 +0100

    Added some water and env. Also colliders

---
+++ Assets/Scripts/AreaTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTrigger : MonoBehaviour {
+	
+	//public Transform explosionPrefab;
+
+	void OnTriggerEnter(Collider other)
+	{
+		//Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+		//Vector3 pos = contact.point;
+		//Instantiate(explosionPrefab, pos, rot);
+		//Destroy(other);
+		Debug.Log("collision");
+	}
+}
