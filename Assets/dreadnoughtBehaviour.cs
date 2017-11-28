825acb181ee553cf1cc881cb52fe3134fd41df80
Fredrik Spansk <fredrik.spansk@gmail.com>
2017-11-28 17:39:29 +0100

    Added snap on map script
    Snap the ship to the map by pressing the button

---
+++ Assets/dreadnoughtBehaviour.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dreadnoughtBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(gameObject.transform.position, -gameObject.transform.up, Color.red);
+
+	}
+
+    public void snapShip ()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(gameObject.transform.position, -gameObject.transform.up, out hit, 100f))
+        {
+            gameObject.transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            gameObject.transform.parent = GameObject.Find("Plane").transform;
+            
+        }
+    }
+}
