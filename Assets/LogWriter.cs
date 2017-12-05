16b9f6469bed8ef4261b0cd3bd7238320928652a
Julia Sporre <>
2017-12-05 14:56:47 +0100

    working chat

--- Assets/LogWriter.cs
+++ Assets/LogWriter.cs
@@ -5,10 +5,91 @@ using System.Collections.Generic;
 using UnityEngine.Networking;
 
 public class LogWriter : NetworkBehaviour {
+	
+	public List<string> messages = new List<string>();
 
+	[SerializeField]
+	private string inputMsg;
+	[SerializeField]
+	private string tmpName;
 
-    [SyncVar(hook = "whoAreYou")]
+	private PlayerToLog player;
+
+	// Use this for initialization
+	void Start () {
+		//To handle out of range index error in init
+		for (int i = 0; i < 10 ; i++) {
+			messages.Add (" ");
+		}
+	}
+
+	public void SetChatPlayer(PlayerToLog p, string name)
+	{
+		Debug.Log ("here");
+		player = p;
+		tmpName = name;
+	}
+
+	public void AddNameAndMsg(string name, string msg)
+	{
+		string str = name + ": "+msg;
+
+		if(isServer){
+			RpcAddMsg(str);
+		}
+	}
+
+	// server -> client
+	[ClientRpc]
+	void RpcAddMsg(string str)
+	{
+		//Adds message to list
+		messages.Add (str);
+		string newMessageString = "";
+
+		//Writes the list to string
+		for (int i = 9; i > 0 ; i--) {
+			newMessageString += "\n " + messages [messages.Count - i];
+		}
+
+		//Adds text to gameObject
+		gameObject.GetComponent<Text>().text = newMessageString;
+	}
+
+	void OnGUI()
+	{
+
+		inputMsg = GUILayout.TextField(inputMsg);
+		if( GUILayout.Button("SEND") ){
+			player.SendMsg(inputMsg);
+			inputMsg = "";
+		}
+
+	}
+
+	public bool CheckSameName(string name)
+	{
+		PlayerToLog[] players = GameObject.FindObjectsOfType<PlayerToLog>();
+		for(int i = 0; i < players.Length; i++){
+			if(players[i].userName == name){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public override void OnNetworkDestroy()
+	{
+		// disconnect
+		Debug.Log("MainUI OnNetworkDestroy");
+		NetworkServer.DisconnectAll();
+	}
+
+   /* //[SyncVar(hook = "whoAreYou")]
     public string syncedString = "A monkey walked into a park";
+
+	public string blabla = "hej";
+
 
     List<string> messages = new List<string>();
 
@@ -27,8 +108,7 @@ public class LogWriter : NetworkBehaviour {
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Space pressed");
-            syncedString = "A MONKEY!!!!!";
+			whoAreYou ();
         }
             
 
@@ -78,5 +158,5 @@ public class LogWriter : NetworkBehaviour {
 
 		//Adds text to gameObject
 		gameObject.GetComponent<Text>().text = newMessageString;
-	}
+	} */
 }
---
+++ Assets/PlayerToLog.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerToLog : NetworkBehaviour {
+
+	[SyncVar]
+	public string userName = "user";
+
+	private LogWriter mainUI;
+
+	// Use this for initialization
+	void Awake () {
+		Debug.Log("Awake");
+	}
+
+	public override void PreStartClient()
+	{
+		Debug.Log("PreStartClient");
+		mainUI = GameObject.FindObjectOfType<LogWriter>();
+	}
+
+
+
+
+	public override void OnStartLocalPlayer()
+	{
+		Debug.Log("OnStartLocalPlayer");
+		string nameStr = isServer ? "user_Server" : "user_Client";
+
+		mainUI.SetChatPlayer(this, nameStr);
+
+		SendName(nameStr);
+		CmdSendToServer("connected.");
+	}
+
+	public bool CheckSameName(string name)
+	{
+		PlayerToLog[] players = GameObject.FindObjectsOfType<PlayerToLog>();
+		for(int i = 0; i < players.Length; i++){
+			if(players[i].userName == name){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public override void OnNetworkDestroy()
+	{
+		Debug.Log("OnNetworkDestroy "+userName);
+		SendMsg("disconnected.");
+	}
+
+	public void SendMsg(string str)
+	{
+		CmdSendToServer(str);
+	}
+
+	public void SendName(string str)
+	{
+		CmdSendName(str);
+	}
+
+	//[Client]
+	public void ChangeName(string str)
+	{
+		if( mainUI.CheckSameName(str) ){
+			SendMsg("same name.");
+			return;
+		}
+		string currentName = userName;
+
+		SendName(str);
+		SendMsg(currentName +" > "+ str + " renamed.");
+	}
+
+	// client player -> server
+	[Command]
+	void CmdSendToServer(string str)
+	{
+		mainUI.AddNameAndMsg(userName, str);
+	}
+
+	[Command]
+	void CmdSendName(string name)
+	{
+		userName = name;
+	}
+}
