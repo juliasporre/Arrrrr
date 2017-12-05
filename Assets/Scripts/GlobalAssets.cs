82f6750e2785b476f06a1bf7c20b340ede668673
Fredrik Spansk <fredrik.spansk@gmail.com>
2017-12-05 11:02:49 +0100

    SyncVarTest

---
+++ Assets/Scripts/GlobalAssets.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PrefabType
+{
+    Player,
+    Npc,
+    SomeOtherNpc
+}
+public class GlobalAssets : MonoBehaviour
+{
+    public GameObject NetworkEntityStatePrototype;
+
+    public GameObject ServerEntityPrototype;
+
+    public GameObject ClientEntityPrefab;
+
+    public GameObject PlayerClientPrefab;
+
+    public GameObject Dreadnought;
+
+    public GameObject Scavenger;
+}
