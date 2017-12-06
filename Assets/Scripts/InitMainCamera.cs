79d78cabfb34962535a9ce845e6f9b4676332e38
Julia Sporre <>
2017-12-06 20:01:42 +0100

    fixed implementation of menu

---
+++ Assets/Scripts/InitMainCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class InitMainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponentInChildren<BackgroundPlaneBehaviour>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
